f8f054b5e966bdd9f60d696dbdf409fba5150fdb
Nicole Hamilton <Nicole@NicolesLaptop.local>
2021-01-07 12:51:07 -0800

    finsihed

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -2,3 +2,36 @@
 # input() always returns a string value. If you ever want someone
 # to enter a number you have to use the `int()` function to convert
 # what they typed in to a string.
+
+### Challenge 1 - Calculator
+
+"""Create a simple calculator that first asks the user what method they would like
+to use (addition, subtraction, multiplication, division) and then asks the user
+for two numbers, returning the result of the method with the two numbers. Here
+is a sample prompt:
+
+```
+What calculation would you like to do? (add, sub, mult, div)
+add
+What is number 1?
+3
+What is number 2?
+6
+Your result is 9
+```"""
+
+operation = input("What calculation woud you like to do? (add, sub, mult, div)?  ")
+num1 = input("What's the first number? ")
+num2 = input("What's the second number?" )
+
+def calc(num1, num2, operation):
+    if operation == "add":
+        return num1 + num2
+    elif operation == "sub":
+        return num1 - num2
+    elif operation == "mult":
+        return num1 * num2
+    else:
+        return num1 / num2
+
+print(calc(int(num1), int(num2), operation))
--- challenges/02-reverse.py
+++ challenges/02-reverse.py
@@ -6,3 +6,30 @@
 # several ways to reverse a string, and it's a good read!
 #
 # http://www.techbeamers.com/essential-python-tips-tricks-programmers/?utm_source=mybridge&utm_medium=blog&utm_campaign=read_more#tip1
+
+"""### Challenge 2 - Reverse a string
+Reverse a string manually. Don't use s[::-1] (even though that's awesome).
+Create a new variable storing an empty string and add the letters from
+the first string one by one. The for loop should iterate over the length
+of the string and you should access letters individually.
+
+Below is some sample output.
+
+```
+Enter a string:
+reverse_me
+em_esrever
+```"""
+
+string_empty = []
+string_forward = "This_is_a_string"
+string_forward.split(" ")
+string_empty[:] = string_forward
+rev_string = " "
+for i in range(len(string_empty)):
+    rev_string += string_empty[len(string_empty) - 1 - i]
+ 
+    
+print(rev_string)    
+
+    
